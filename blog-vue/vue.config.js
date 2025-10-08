0041582a539836641d576130e75d9ecf0109a68c
marko379 <markogojkovic045@gmail.com>
2025-10-08 10:44:29 +0100

    static files updated

--- blog-vue/vue.config.js
+++ blog-vue/vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+const path = require('path');
+
+module.exports = {
+  outputDir: path.resolve(__dirname, '../blog/static'), // where built JS/CSS go
+  indexPath: path.resolve(__dirname, '../blog/templates/index.html'), // where built HTML goes
+  publicPath: '/static/', // make all asset URLs work with Django’s static handling
+  transpileDependencies: true,
+};
