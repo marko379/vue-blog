e65188b4d35d51205e8bd2d8507dc3d74492dabd
marko379 <markogojkovic045@gmail.com>
2025-10-09 12:30:13 +0100

    Deploy Vue build for production 2nd time

--- blog/settings.py
+++ blog/settings.py
@@ -87,7 +87,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [],
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'blog', 'templates'), 
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
