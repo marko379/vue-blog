71b74a95ba763eba071a5f9b124c8df73dcfc51b
marko379 <markogojkovic045@gmail.com>
2025-11-01 10:55:58 +0000

    fix settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -25,11 +25,10 @@ if os.getenv('RAILWAY_ENVIRONMENT'):
     CLOUDINARY_STORAGE = {
         'CLOUD_NAME': os.getenv('CLOUD_NAME'),
         'API_KEY': os.getenv('CLOUD_API_KEY'),
-        'API_SECRET': os.getenv('CLOUD_API_SECRET'),
+        'API_SECRET': os.getenv('CLOUD_API_SECRET')
     }
 
-
- DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
