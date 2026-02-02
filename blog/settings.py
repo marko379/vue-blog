19d9cca13aa51b6a184a913958ade77ae76b6297
marko379 <markogojkovic045@gmail.com>
2026-02-02 22:38:05 +0000

    update settings CORS

--- blog/settings.py
+++ blog/settings.py
@@ -27,6 +27,7 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://vue-blog-production.up.railway.app",
+    "https://web-production-2f974.up.railway.app"
 ]
 
 # -------------------------
@@ -96,6 +97,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https://vue-blog-production.up.railway.app",
+    "https://web-production-2f974.up.railway.app"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
