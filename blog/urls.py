7a426fc159bb5232f37bd1918a33081e5028b1ed
marko379 <markogojkovic045@gmail.com>
2026-02-05 17:44:29 +0000

    add from django.views.generic import TemplateView

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
